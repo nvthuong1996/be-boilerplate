3703529df4f79ea982f195e5d023bb9a116a9671
Thuong Nguyen Van <nvthuong1996@gmail.com>
2020-12-13 10:30:03 +0700

    init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('abcd')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('abcd')
   getHello(): string {
     return this.appService.getHello();
   }
